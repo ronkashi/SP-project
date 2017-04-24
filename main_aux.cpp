f63c353251c2f7afcb53644682cd88c6c81a2314
Gavriel Cohen <gavrielc@Gavrielc-mbp15.local>
2017-04-24 13:17:09 +0300

    2

--- main_aux.cpp
+++ main_aux.cpp
@@ -7,7 +7,6 @@
 #define DEFAULT_CONFIG_ERROR "The default configuration file spcbir.config couldn't be open\n"
 #define CONFIG_ERROR "The configuration file %s couldn't be open\n"
 #define INVALID_COMMAND "Invalid command line: use -c <config_filename>\n"
-#define CONFIG_ALLOC_ERROR "Config memmory allocation failure\n"
 
 SPConfig createConfig(int argc, char* argv[], SP_CONFIG_MSG* msg) {
 	SPConfig config;
@@ -15,21 +14,14 @@ SPConfig createConfig(int argc, char* argv[], SP_CONFIG_MSG* msg) {
         config = spConfigCreate(DEFAULT_CONFIG, msg);
         if(*msg == SP_CONFIG_CANNOT_OPEN_FILE) {
             printf(DEFAULT_CONFIG_ERROR);
-            return NULL;
         }
     } else if(argc == 3 && (strcmp(argv[1], "-c") == 0)) {
         config = spConfigCreate(argv[2], msg);
         if(*msg == SP_CONFIG_CANNOT_OPEN_FILE) {
             printf(CONFIG_ERROR, argv[2]);
-            return NULL;
         }
     } else {
         printf(INVALID_COMMAND);
-        return NULL;
-    }
-
-    if(*msg != SP_CONFIG_SUCCESS) {
-        printf(CONFIG_ALLOC_ERROR);
         return NULL;
     }
 	return config;
