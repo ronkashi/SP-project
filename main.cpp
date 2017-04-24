76de9d4a62d2059923eea43a929672ce91691d2e
Gavriel Cohen <gavrielc@Gavrielc-mbp15.local>
2017-04-24 19:17:14 +0300

    21

--- main.cpp
+++ main.cpp
@@ -10,22 +10,32 @@ int main(int argc, char* argv[]) {
     SPPoint** flatDatabase;
     kdTreeNode* kdTree = NULL;
     int allFeatures=0;
-    sp::ImageProc ip;
+    sp::ImageProc* ip;
 
     config = createConfig(argc, argv);
     if(!config) return -1;
 
-    if(!initializeLogger(config)) return -1;
+    if(!initializeLogger(config)) {
+        spConfigDestroy(config);
+        return -1;
+    }
+
+    ip = (sp::ImageProc*) malloc(sizeof(*ip));
+    if(!ip) {
+        spLoggerPrintError(ALLOC_ERROR_MSG, __FILE__, __func__, __LINE__);
+        spConfigDestroy(config);
+    }
     try {
-        ip = sp::ImageProc(config);
+        *ip = sp::ImageProc(config);
     } catch(...) {
+        free(ip);
         spLoggerDestroy();
         spConfigDestroy(config);
         return -1;
     }
     
 
-    flatDatabase = processFeatures(config, &allFeatures, ip);
+    flatDatabase = processFeatures(config, &allFeatures, *ip);
     if(!flatDatabase) {
         spConfigDestroy(config);
         spLoggerDestroy();
@@ -40,9 +50,10 @@ int main(int argc, char* argv[]) {
 		return -1;
     }
 
-    searchByImage(config, kdTree, ip);
+    searchByImage(config, kdTree, *ip);
 
     //TODO destroy tree
+    free(ip);
     spConfigDestroy(config);
     spLoggerDestroy();
     printf("Exiting...\n");
